1dca86aeffeda4c01f1af181dda68a8c96a4e964
edimdendidiwangga <edimdendy@gmail.com>
2017-06-19 19:47:35 +0700

    commit-pertama-baru-bar

--- script.js
+++ script.js
@@ -4,7 +4,9 @@
 const width = 750,
   height = 300,
   margin = 20
-marginLeft = 40
+  marginLeft = 40
+
+
 
 // Drawing area
 let svg = d3.select('#results')
@@ -15,12 +17,44 @@ let svg = d3.select('#results')
 // Data reloading
 let reload = () => {
   // Your data parsing here...
+  d3.tsv('afcw-results.tsv', (rows)=>{
+    // console.log(rows);
+      redraw(rows)
+  })
 }
 
 // redraw function
 let redraw = (data) => {
   // Your data to graph here
+  console.log(data);
 
+  var yScale = d3.scaleLinear()
+    .domain([0, 6])
+    .range([0, 300])
+
+  var x_axis = d3.axisBottom()
+  .scale(yScale)
+
+  svg.selectAll('rect')
+  .data(data)
+  .enter()
+  .append('rect')
+  .attr('class', 'bar')
+  .attr('x', (d,i) =>{
+    return i*12
+  })
+  .attr('y', (d)=>{
+    return height - yScale(d.GoalsScored)
+  })
+  .attr('width', 10)
+  .attr('height', (d)=>{
+    return d.GoalsScored * 50
+  })
+  .attr('fill', 'red')
+
+svg
+  .append('g')
+  .call(x_axis)
 }
 
 reload()
